224be7455ebf16b134c77469c7b7571e55529f56
lukasEl11 <lukas.stasak@elevon.io>
2024-10-23 16:13:14 +0200

    feat: add & config mongoose

--- product-service/src/express/env-vars.ts
+++ product-service/src/express/env-vars.ts
@@ -8,4 +8,7 @@ export default {
   AppVersion: process.env.APP_VERSION ?? 'x.y.z',
   NodeEnv: process.env.NODE_ENV ?? '',
   Port: process.env.PORT ?? 0,
+  db: {
+    MONGO_URI: process.env.MONGO_URI ?? '',
+  },
 } as const;
--- product-service/src/index.ts
+++ product-service/src/index.ts
@@ -1,18 +1,23 @@
 import './express/pre-start'; // Must be the first import
 
 import express from 'express';
+import logger from 'jet-logger';
 import env from './express/env-vars';
 import ProductsRouter from './routes/productRoutes';
+import connectDB from './config/db';
 
 const app = express();
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
+// Connect to MongoDB
+connectDB();
+
 // Route handler
 app.use('/api', ProductsRouter);
 
 // Start the server
 app.listen(env.Port, () => {
-  console.log(`Server is running on http://localhost:${env.Port}`);
+  logger.info(`Server is running on http://localhost:${env.Port}`);
 });
